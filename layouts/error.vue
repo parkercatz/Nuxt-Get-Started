27a5bb3fe960e753bc4f040ff6bbf5f42d533f9e
ParkerCatz <tksm.webdesign09@gmail.com>
2021-03-21 23:13:27 +0900

    fix: error page style

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>Error</div>
+  <div class="container">
+    <p class="error-text">404</p>
+    <div>
+      <span>Take me</span>
+      <nuxt-link to="/">Home</nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+.container {
+  height: viewport;
+  background: aqua;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+.error-text {
+  font-size: 130px;
+}
+</style>
